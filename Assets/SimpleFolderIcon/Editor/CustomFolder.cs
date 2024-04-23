3195352bd7064cb455409f7ed6c6e4c3715562e0
S0iRu <108854997+S0iRu@users.noreply.github.com>
2024-04-23 15:03:00 +0900

    Update CustomFolder.cs
    部分一致に修正

--- Assets/SimpleFolderIcon/Editor/CustomFolder.cs
+++ Assets/SimpleFolderIcon/Editor/CustomFolder.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -13,6 +16,7 @@ namespace SimpleFolderIcon.Editor
             EditorApplication.projectWindowItemOnGUI += DrawFolderIcon;
         }
 
+        // 部分一致に修正
         static void DrawFolderIcon(string guid, Rect rect)
         {
             var path = AssetDatabase.GUIDToAssetPath(guid);
@@ -20,8 +24,22 @@ namespace SimpleFolderIcon.Editor
 
             if (path == "" ||
                 Event.current.type != EventType.Repaint ||
-                !File.GetAttributes(path).HasFlag(FileAttributes.Directory) ||
-                !iconDictionary.ContainsKey(Path.GetFileName(path)))
+                !File.GetAttributes(path).HasFlag(FileAttributes.Directory))
+            {
+                return;
+            }
+
+            var folderName = Path.GetFileName(path);
+
+            var matchingIcon = iconDictionary.FirstOrDefault(pair => folderName.IndexOf(pair.Key, StringComparison.OrdinalIgnoreCase) >= 0);
+
+            if (matchingIcon.Equals(default(KeyValuePair<string, Texture>)))
+            {
+                return;
+            }
+
+            var texture = matchingIcon.Value;
+            if (texture == null)
             {
                 return;
             }
@@ -41,13 +59,8 @@ namespace SimpleFolderIcon.Editor
                 imageRect = new Rect(rect.x + 2, rect.y - 1, rect.height + 2, rect.height + 2);
             }
 
-            var texture = IconDictionaryCreator.IconDictionary[Path.GetFileName(path)];
-            if (texture == null)
-            {
-                return;
-            }
-
             GUI.DrawTexture(imageRect, texture);
         }
+
     }
 }
